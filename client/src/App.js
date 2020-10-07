94644ccdd144c27f099034f1537bfa559cc579aa
cseibert2667 <cseibert2667@gmail.com>
2020-10-07 13:52:35 -0700

    header component built w/ navbar & header

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Saved from "./pages/Saved";
+import Search from "./pages/Search";
+import Header from "./components/Header/Header";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Search} />
+        <Route exact path="/saved" component={Saved} />
+      </Switch>
+    </Router>
   );
 }
-
 
 export default App;
---
+++ client/src/pages/Search.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Nav from "../components/Header/Header"
+
+export default function Search() {
+    return (
+        <div>
+            <h1>hello</h1>
+        </div>
+    )
+}
