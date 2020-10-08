1f1a504d24100704572f6711376e9b30dd9afe10
cseibert2667 <cseibert2667@gmail.com>
2020-10-08 09:36:14 -0700

    results & book components added, need styling

--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Container from "react-bootstrap/Container"
+import Container from "react-bootstrap/Container";
+import Book from "../Book/Book"
 
 export default function Results() {
     return (
         <Container>
             <h3>Results</h3>
+            <Book/>
         </Container>
     )
 }
